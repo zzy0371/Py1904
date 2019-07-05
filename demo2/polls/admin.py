29dc5449d2aec9f38bbcc9be0bb496b46685758e
zzy0371 <496575233@qq.com>
2019-07-05 15:27:28 +0800

    关联查询

--- demo2/polls/admin.py
+++ demo2/polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question,Choice
+from .models import Question,Choice,Temp
 # Register your models here.
 class ChoiceInline(admin.StackedInline):
     model = Choice
@@ -10,3 +10,4 @@ class QuestionAdmin(admin.ModelAdmin):
 
 admin.site.register(Question,QuestionAdmin)
 admin.site.register(Choice)
+admin.site.register(Temp)
--- demo2/polls/models.py
+++ demo2/polls/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Question(models.Model):
     desc = models.CharField(max_length=20)
@@ -15,4 +15,30 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.desc
+
+class Temp(models.Model):
+    title = models.CharField(verbose_name="标题", help_text="标题", max_length=20)
+    desc = models.CharField(help_text="描述", max_length=20,null=True,blank=True,db_column="描述")
+
+class MyUser(models.Model):
+    telephone = models.CharField(max_length=11,)
+    default_user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Account(models.Model):
+    username = models.CharField(max_length=20)
+
+class Contact(models.Model):
+    telephone = models.CharField(max_length=20)
+    acc = models.OneToOneField(Account,on_delete=models.CASCADE)
+
+class Host(models.Model):
+    name = models.CharField(max_length=20)
+
+class Application(models.Model):
+    name = models.CharField(max_length=20)
+    h = models.ManyToManyField(Host)
+
+
+
 
