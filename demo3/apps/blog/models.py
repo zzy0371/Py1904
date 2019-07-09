307b4ba9210d40095ea61a7ab1833da9be6df05f
zzy0371 <496575233@qq.com>
2019-07-09 16:52:43 +0800

    博客数据模型

---
+++ demo3/apps/blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Ads(models.Model):
+    img = models.ImageField(upload_to="ads")
+    desc = models.CharField(max_length=20)
+    index = models.IntegerField(default=0)
+
+class Category(models.Model):
+    title = models.CharField(max_length=20)
+
+class Tag(models.Model):
+    title = models.CharField(max_length=10)
+
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    views = models.IntegerField(default=0)
+    body = models.TextField()
+    tags = models.ManyToManyField(Tag)
+
+
+
+
